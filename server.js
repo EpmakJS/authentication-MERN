e138113416e0887555e3b4fe3eb6bde57daee438
Viktor Ermakov <ermakov.viktor98@gmail.com>
2019-03-29 21:38:51 +0300

    Shopping List - Done

--- server.js
+++ server.js
@@ -17,6 +17,15 @@ mongoose
 
 app.use('/api/items', items);
 
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server stared in port ${port}`));
